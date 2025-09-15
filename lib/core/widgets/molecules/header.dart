c45f439053e9a21073f9d24b9495a947c587c102
webyosupernow@gmail.com <webyosupernow@gmail.com>
2025-09-15 22:17:23 +0900

    ヘッダーウィジェットにタイトルプロパティを追加し、AppBarのタイトルを動的に変更可能に

--- lib/core/widgets/molecules/header.dart
+++ lib/core/widgets/molecules/header.dart
@@ -2,15 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class Header extends ConsumerWidget implements PreferredSizeWidget {
-  const Header({super.key});
+  const Header(
+    {
+        super.key,
+        this.title = '城なび'
+    }
+  );
+
+  final String title; // ← 受け取った値を保持
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return AppBar(
-      title: Text('Header'),
+      centerTitle: false, // タイトルを左寄せ
+      title: Text(title),
+      actions: [
+        Padding(
+            padding: const EdgeInsets.only(right: 8.0), // 右に余白
+            child: IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () {
+                Scaffold.of(context).openEndDrawer();
+            },
+            ),
+        ),
+      ],
     );
   }
 
   @override
+  // kToolbarHeight:Flutter が定義している定数で、デフォルトの AppBar の高さ（56.0）
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
